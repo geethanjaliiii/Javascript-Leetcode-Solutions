62191a87b7e953d8c1d7d6880c1e348f8cadcb91
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-10-23 09:20:19 +0530

    Time: 67 ms (97.37%), Space: 58.1 MB (5.03%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.js
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.js
@@ -4,19 +4,10 @@
  * @return {number}
  */
 var buyChoco = function (prices, money) {
-    let spend = 0
-    for (let i = 0; i < prices.length; i++) {
-        
-        for (let j = i + 1; j < prices.length; j++) {
-            if (!spend && prices[i] + prices[j] <= money) {
-                spend = prices[i] + prices[j]
-            }
-            else if (prices[i] + prices[j] <= money && prices[i]+prices[j]<spend){
-                spend=prices[i]+prices[j]
-            }
-
-            }
-        
-    }
-    return money - spend
+   prices.sort((a,b)=>a-b)
+   console.log(prices)
+   if(prices[0]+prices[1]<=money){
+    return money-(prices[0]+prices[1])
+   }
+   return money
 };
