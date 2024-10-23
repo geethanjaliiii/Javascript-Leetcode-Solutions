02e0706155ad1e46109ac54f492db876f7d9eccd
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-10-23 09:29:12 +0530

    Time: 10 ms (93.58%), Space: 51.6 MB (21.37%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.js
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var countNegatives = function(grid) {
-   let arr= grid.flat(1)
-   arr.sort((a,b)=>a-b)
-   let count=0
-   for(let i=0;i<arr.length;i++){
-    if(arr[i]<0){
-        count++
-    }
-   }
-   return count
+ return grid.flat().filter((e)=>e<0).length
 };
