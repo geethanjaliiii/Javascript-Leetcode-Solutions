011ffef9aeb7ee9381b67350a029c99995074315
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-10-23 13:44:09 +0530

    Time: 0 ms (100%), Space: 53 MB (28.66%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -4,10 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===target){
-            return i
-        }
+ let low = 0
+ let high = nums.length-1
+ while(low<=high){
+    let mid=low+Math.floor((high-low)/2)
+    if(nums[mid]===target){
+        return mid
+    }else if(nums[mid]<target){
+        //no need to seach before curr middle element
+        low=mid+1
+    }else {
+        high=mid-1
     }
-    return -1
+ }
+ return -1
 };
