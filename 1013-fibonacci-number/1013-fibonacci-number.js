98053a103c93d943c0e21f40b6a048c1edcc9ee0
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-10-23 10:10:48 +0530

    Time: 58 ms (50.66%), Space: 48.8 MB (36.71%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var fib = function(n) {
-    let  prev=0;
+    let prev=0
     let curr=1
-    let next=prev+curr
-    if(n<=1){
+    let next
+    if(n<2){
         return n
     }
     for(let i=2;i<=n;i++){
         next=prev+curr
-       prev=curr
-       curr=next
-    
-       
+        prev=curr
+        curr=next
     }
     return next
 };
