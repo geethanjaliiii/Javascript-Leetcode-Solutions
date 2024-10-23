22b5d75b8bb6dbd83bf3e1c9c898a81baab387c1
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-10-23 12:31:00 +0530

    Time: 0 ms (100%), Space: 49.6 MB (13.17%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -8,7 +8,7 @@ var searchInsert = function(nums, target) {
     for(let i=0;i<nums.length;i++){
         if(nums[i]===target){
             return i
-        }else if(nums[i]<target && !nums.includes(target)){
+        }else if(nums[i]<target){
             index = i+1
 
     }}
