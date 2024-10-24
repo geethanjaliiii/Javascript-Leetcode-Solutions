408107418c947888a0df5b2b4574b18250c31732
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-10-24 14:38:37 +0530

    Time: 4 ms (99.78%), Space: 54 MB (21.98%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.js
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.js
@@ -4,22 +4,37 @@
  * @return {string}
  */
 var kthDistinct = function(arr, k) {
-    let dis=[]
-    for(let i=0;i<arr.length;i++){
-        let isUni=1
-        for(let j=0;j<arr.length;j++){
-            if(i!=j && arr[i]==arr[j]){
-                isUni=0
-              break
-            }
-        }
-        if(isUni){
-            dis.push(arr[i])
+    let freq={}
+    let uni=[]
+    for(let i of arr){
+        freq[i]=(freq[i] || 0)+1
+    }
+    for(let ele of arr){
+        if(freq[ele]===1){
+           uni.push(ele)
         }
     }
-    console.log(dis)
-    if(k<=dis.length){
-        return dis[k-1]
+    if(k<=uni.length){
+        return uni[k-1]
+    }else{
+        return ""
     }
-   return ""
+//     let dis=[]
+//     for(let i=0;i<arr.length;i++){
+//         let isUni=1
+//         for(let j=0;j<arr.length;j++){
+//             if(i!=j && arr[i]==arr[j]){
+//                 isUni=0
+//               break
+//             }
+//         }
+//         if(isUni){
+//             dis.push(arr[i])
+//         }
+//     }
+//     console.log(dis)
+//     if(k<=dis.length){
+//         return dis[k-1]
+//     }
+//    return ""
 };
