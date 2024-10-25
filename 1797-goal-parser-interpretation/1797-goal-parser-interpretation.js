82058a730c31520958a33c425755a31f6aed3efd
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-10-25 20:42:25 +0530

    Time: 56 ms (35.29%), Space: 49.1 MB (34.73%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.js
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.js
@@ -3,18 +3,19 @@
  * @return {string}
  */
 var interpret = function(command) {
-    let arr=command.split('')
-    let str=""
-     console.log(arr)
-     for(let i=0;i<arr.length;i++){
-        if(arr[i]=='G'){
-            str+="G"
-        }else if(arr[i]=='(' && arr[i+1]===')'){
-            str+='o'
-        }else if(arr[i]==='(' && arr[i+1]==='a'){
-            str+='al'
-        }
-     }
-     return str
+    return command.replaceAll('()','o').replaceAll('(al)','al')
+    // let arr=command.split('')
+    // let str=""
+    //  console.log(arr)
+    //  for(let i=0;i<arr.length;i++){
+    //     if(arr[i]=='G'){
+    //         str+="G"
+    //     }else if(arr[i]=='(' && arr[i+1]===')'){
+    //         str+='o'
+    //     }else if(arr[i]==='(' && arr[i+1]==='a'){
+    //         str+='al'
+    //     }
+    //  }
+    //  return str
 
 };
