1b955df22863f3b367fee8368b8c144b03580d62
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-11-10 21:29:10 +0530

    Time: 4 ms (36.09%), Space: 49.9 MB (98.49%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.js
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.js
@@ -3,12 +3,28 @@
  * @return {number}
  */
 var mostWordsFound = function(sentences) {
+    // let max=0
+    // for(let i=0;i<sentences.length;i++){
+    //     console.log(i)
+    //     let count=sentences[i].split(" ").length
+    //     if(count>max){
+    //         max=count
+    //     }
+    // }
+    // return max
+    
+
+    //bruteforce approach
     let max=0
     for(let i=0;i<sentences.length;i++){
-        console.log(i)
-        let count=sentences[i].split(" ").length
-        if(count>max){
-            max=count
+        let count=0
+        for(let j=0;j<sentences[i].length;j++){
+            if(sentences[i].charAt(j)===' '){
+             count++
+            }
+        }
+        if(count+1>max){
+            max=count+1
         }
     }
     return max
