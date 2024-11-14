850a88615c1d06bfca254e3ea7de870a4ccc759c
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-11-14 22:11:37 +0530

    Time: 15 ms (66.66%), Space: 53.6 MB (48.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,7 +7,17 @@ var isAnagram = function(s, t) {
     if(s.length!=t.length){
         return false
     }
-  let sortedS=s.split("").sort().join("")
-  let sortedT=t.split("").sort().join("")
-    return sortedS===sortedT
+    let count={}
+    for(let i of s){
+        count[i]=(count[i]||0)+1
+    }
+    for(let i of t){
+        if(!count[i]) return false
+
+       count[i]-=1
+    }
+    return true
+//   let sortedS=s.split("").sort().join("")
+//   let sortedT=t.split("").sort().join("")
+//     return sortedS===sortedT
 };
