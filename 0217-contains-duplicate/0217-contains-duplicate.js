d75205d578fe2f1055bc38032d49a11b725bc6d4
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-11-15 07:45:22 +0530

    Time: 100 ms (5.07%), Space: 59.5 MB (93.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,25 +3,9 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    //approach 1
-// nums.sort((a,b)=>a-b)
-
-//     for(let i=0;i<nums.length;i++){
-//         if(nums[i]===nums[i+1]){
-//             return true
-//         }
-//     }
-//     return false
-
-//approach 2
-let count={}
 nums.sort()
-for(let i of nums){
-count[i]=(count[i]||0)+1
-}
-console.log(count)
-for(let i of nums){
-    if(count[i]>1){
+for(let i=0;i<nums.length;i++){
+    if(nums[i]==nums[i+1]){
         return true
     }
 }
