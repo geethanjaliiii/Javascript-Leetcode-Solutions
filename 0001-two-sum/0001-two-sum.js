fce98394482ac3c26d87672c4094e86a012056ca
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-12-09 18:28:38 +0530

    Time: 0 ms (100%), Space: 51.5 MB (28.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let res =[]
-    for(let i=0;i<nums.length;i++){
-        for(let j=0;j<nums.length;j++){
-            if(i!=j && nums[i]+nums[j]===target){
-                res.push(i);
-                res.push(j)
-               
-            }
-        }
+let obj={}
+for(let i=0;i<nums.length;i++){
+    if(obj[target-nums[i]]>=0){
+        return [obj[target-nums[i]],i]
+    }else{
+        obj[nums[i]]=i
     }
-    
-     return [...new Set(res)]
+}
 };
