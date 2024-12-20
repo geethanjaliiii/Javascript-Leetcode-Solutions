ff84e37acf3d012b97d91769e13d8fee7decc019
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-12-20 22:19:23 +0530

    Time: 5 ms (80.21%), Space: 55.7 MB (38.21%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -6,7 +6,7 @@ var romanToInt = function(s) {
     let rom={'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
     let int=0
     for(let i=0;i<s.length;i++){
-        if(i+1<s.length && rom[s[i]]<rom[s[i+1]]){
+        if( rom[s[i]]<rom[s[i+1]]){
             int-=rom[s[i]]
         }else{
           int+=rom[s[i]]
