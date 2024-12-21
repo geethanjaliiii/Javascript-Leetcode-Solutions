b7b76be4e471f8f371402104adb380ff7f6fd3b6
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-12-21 11:24:26 +0530

    Time: 1 ms (76.62%), Space: 48.1 MB (99.33%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,20 +4,30 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    let stack=[]
+    let sp=0;
+    let tp=0;
+    while(sp<s.length && tp<t.length){
+        if(s[sp]===t[tp]){
+            sp++
+        }
+        tp++
+    }
+    return sp===s.length
+    //using stack
+    // let stack=[]
    
-    for(let i=t.length-1;i>=0;i--){
-          stack.push(t[i])
-    }
-    let i=0
-    let found=0
-    while(stack.length && i<s.length){
-        console.log(stack)
-     if(s[i]==stack.pop()){
-        i++
-        found++
-     }
-    }
-    console.log(stack,found)
-    return found===s.length
+    // for(let i=t.length-1;i>=0;i--){
+    //       stack.push(t[i])
+    // }
+    // let i=0
+    // let found=0
+    // while(stack.length && i<s.length){
+    //     console.log(stack)
+    //  if(s[i]==stack.pop()){
+    //     i++
+    //     found++
+    //  }
+    // }
+    // console.log(stack,found)
+    // return found===s.length
 };
