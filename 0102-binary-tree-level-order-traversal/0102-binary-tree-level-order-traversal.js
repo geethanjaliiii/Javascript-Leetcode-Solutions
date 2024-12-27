34fe92ba41f362661cdd0ad7ccacecded4339dcc
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-12-27 10:41:30 +0530

    Time: 4 ms (28.22%), Space: 54.5 MB (84.13%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    if (!root) return []
+    let result = []
+    let queue = [root]
+    while (queue.length) {
+        let level = []
+        let queLen = queue.length //storing current level length safely
+        for (let i = 0; i < queLen; i++) {
+            let currentNode = queue.shift()
+            level.push(currentNode.val)
+            if (currentNode.left) queue.push(currentNode.left)
+            if (currentNode.right) queue.push(currentNode.right)
+        }
+        result.push(level)
+    }
+    return result
+};
