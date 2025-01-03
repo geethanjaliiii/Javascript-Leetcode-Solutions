b14883840e060b8a18a5f3ee37ba10a762f8c2a9
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2025-01-03 06:15:42 +0530

    Time: 65 ms (33.26%), Space: 51.8 MB (51.88%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -12,22 +12,24 @@
  */
 
 
-var cloneGraph = function(node) {
-    if(!node) return null
+var cloneGraph = function (node) {
+    if (!node) return null
     //to keep track of already explored neighbouring nodes
-    let clonedMap=new Map()
-   var dfs=(currentNode)=>{
- //if we have already explored the node,just return it
- if(clonedMap.has(currentNode)){
-    return clonedMap.get(currentNode)
- }
- //if not explored, create a clone
- let clone=new Node(currentNode.val)
- clonedMap.set(currentNode,clone)
- for(let neighbor of currentNode.neighbors){
-     clone.neighbors.push(dfs(neighbor))
- }
- return clone
-}
-return dfs(node)
+    let clonedMap = new Map()
+
+    //helper function for dfs cloning
+    var dfs = (currentNode) => {
+        //if we have already explored the node,just return it
+        if (clonedMap.has(currentNode)) {
+            return clonedMap.get(currentNode)
+        }
+        //if not explored, create a clone
+        let clone = new Node(currentNode.val)
+        clonedMap.set(currentNode, clone)
+        for (let neighbor of currentNode.neighbors) {
+            clone.neighbors.push(dfs(neighbor))
+        }
+        return clone
+    }
+    return dfs(node)
 };
