2569b38835a7560618eddbddfbf164068e082bdf
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2025-01-03 06:10:47 +0530

    Time: 75 ms (5.21%), Space: 50.8 MB (98.73%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.js
@@ -0,0 +1,33 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+
+
+var cloneGraph = function(node) {
+    if(!node) return null
+    let clonedMap=new Map()
+   
+return dfs(node,clonedMap)
+};
+var dfs=(currentNode,clonedMap)=>{
+ //if we have already explored the node,just return it
+ if(clonedMap.has(currentNode)){
+    return clonedMap.get(currentNode)
+ }
+ //if not explored, create a clone
+ let clone=new Node(currentNode.val)
+ clonedMap.set(currentNode,clone)
+ for(let neighbor of currentNode.neighbors){
+     clone.neighbors.push(dfs(neighbor,clonedMap))
+ }
+ return clone
+}
