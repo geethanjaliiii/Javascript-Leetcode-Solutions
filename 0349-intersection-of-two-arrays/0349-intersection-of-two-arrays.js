5cc9307b48f33c59ebeb56bf673e9a17627684dd
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2025-01-18 14:58:43 +0530

    Time: 15 ms (5.27%), Space: 51.3 MB (27.86%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,11 +4,33 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-  let arr=[]
-  for(let num of nums1){
-    if(nums2.includes(num)){
-        arr.push(num)
+//   let arr=[]
+//   for(let num of nums1){
+//     if(nums2.includes(num)){
+//         arr.push(num)
+//     }
+//   }
+//   return [...new Set(arr)]
+let arr=[]
+let result=[]
+   for(i=0;i<nums1.length;i++){
+    for(j=0;j<nums2.length;j++){
+         if(nums1[i]==nums2[j]){
+            arr.push(nums1[i])
+         }
     }
-  }
-  return [...new Set(arr)]
+   }
+   for(i=0;i<arr.length;i++){
+    let count=0
+    for(j=i+1;j<arr.length;j++){
+     if(arr[i]==arr[j]){
+        count++
+     }
+    }
+    if(count==0){
+      result.push(arr[i])
+    }
+   }
+   return  result
+
 };
