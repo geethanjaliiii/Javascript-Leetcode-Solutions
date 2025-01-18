f3533be4ee17e5f43ff593257383c9b8ddd13b21
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2025-01-18 15:18:33 +0530

    Time: 0 ms (100%), Space: 50.6 MB (70.09%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,15 +4,15 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-//   let arr=[]
-//   for(let num of nums1){
-//     if(nums2.includes(num)){
-//         arr.push(num)
-//     }
-//   }
-//   return [...new Set(arr)]
-let set1=new Set(nums1)
-let set2=new Set(nums2)
- return [...new Set([...set1].filter(num=>set2.has(num)))]
+  let arr=[]
+  for(let num of nums1){
+    if(nums2.includes(num)){
+        arr.push(num)
+    }
+  }
+  return [...new Set(arr)]
+// let set1=new Set(nums1)
+// let set2=new Set(nums2)
+//  return [...new Set([...set1].filter(num=>set2.has(num)))]
 
 };
