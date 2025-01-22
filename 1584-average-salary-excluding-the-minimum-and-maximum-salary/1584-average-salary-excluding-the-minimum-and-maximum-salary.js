f696c780e792d8043f47a601c9f50a3fb1a6d3d8
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2025-01-22 21:21:31 +0530

    Time: 0 ms (100%), Space: 48.8 MB (68.22%) - LeetHub

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.js
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -3,24 +3,17 @@
  * @return {number}
  */
 var average = function(salary) {
-    //let sum=0
+    
     let max=Math.max(...salary)
     let min=Math.min(...salary)
-    // for(let i=0;i<salary.length;i++){
-    //     if(salary[i]!=max && salary[i]!=min){
-    //         sum+=salary[i]
-    //     }
-    // }
-    // console.log(sum)
-    // return Math.floor(sum/(salary.length-2))
+ 
     let count=0
-    let sum= salary.reduce((acc,curr)=>{
+    return salary.reduce((acc,curr)=>{
     if(curr!=max&& curr!=min){
         acc+=curr
         count++
     }
     return acc
-   },0)
-   console.log(max,min)
-   return sum/count
+   },0)/(count)
+   
 };
