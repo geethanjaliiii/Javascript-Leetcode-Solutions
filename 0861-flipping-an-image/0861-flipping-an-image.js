125b0cee88faf1edb7dc37ad1f241c64e010ad27
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2025-02-12 16:22:07 +0530

    Time: 2 ms (35.18%), Space: 52.6 MB (25.66%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.js
+++ 0861-flipping-an-image/0861-flipping-an-image.js
@@ -2,19 +2,19 @@
  * @param {number[][]} image
  * @return {number[][]}
  */
-var flipAndInvertImage = function(image) {
- 
-    for(let i=0;i<image.length;i++){
-       let start=0;
-        let end=image[i].length-1;
-    
-        while(start<=end){
-           
-           [image[i][start],image[i][end]]=[1-image[i][end],1-image[i][start]]
+var flipAndInvertImage = function (image) {
+
+    for (let i = 0; i < image.length; i++) {
+        let start = 0;
+        let end = image[i].length - 1;
+
+        while (start <= end) {
+
+            [image[i][start], image[i][end]] = [1 - image[i][end], 1 - image[i][start]]
             start++
             end--
         }
     }
 
-   return image
+    return image
 };
